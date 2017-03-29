1d505fecde5d2ab1bd8c3a87d4bab14c3304e4b8
Marlene Oliveira <marleneoliveira.w@gmail.com>
2017-03-29 22:20:00 +0100

    First commit (without functionality).

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/AngularMaterial/pen/bEGJdd.
+
+ Shell w/ Angular Material (v1.1.0)
---
+++ js/index.js
@@ -0,0 +1,10 @@
+// TEA - Time Estimation App
+
+angular.module('TEA', ['ngMaterial'])
+    .controller('TEAController', function ($scope, $log) {
+
+    });
+/*
+Copyright 2016 Google Inc. All Rights Reserved. 
+Use of this source code is governed by an MIT-style license that can be in foundin the LICENSE file at http://material.angularjs.org/license.
+*/
