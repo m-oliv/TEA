3f7952b3bb28b4771410f4f59dba8f75ac0e0a72
Marlene Oliveira <marleneoliveira.w@gmail.com>
2017-04-06 22:21:09 +0100

    - Added variables to index.js. - Added h1 title to the page (might not be needed).

--- js/index.js
+++ js/index.js
@@ -2,7 +2,11 @@
 
 angular.module('TEA', ['ngMaterial'])
     .controller('TEAController', function ($scope, $log) {
+        $scope.bestCaseGuess = 0.0;
+        $scope.middleGroundGuess = 0.0;
+        $scope.worstCaseGuess = 0.0;
 
+        
     });
 /*
 Copyright 2016 Google Inc. All Rights Reserved. 
