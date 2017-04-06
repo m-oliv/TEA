b204157025238fdf2e0253f7dc72910329674023
Marlene Oliveira <marleneoliveira.w@gmail.com>
2017-04-06 23:23:50 +0100

    - Removed the estimate button. - Removed the close card button. - The info is now condensed in a single card. - Working on the align of the inputs and text info in the card (WIP).

--- js/index.js
+++ js/index.js
@@ -5,8 +5,27 @@ angular.module('TEA', ['ngMaterial'])
         $scope.bestCaseGuess = 0.0;
         $scope.middleGroundGuess = 0.0;
         $scope.worstCaseGuess = 0.0;
+        $scope.estimation = 0.0;
+        $scope.showEstimation = false;
 
+        $scope.onEstimateClick = function (best, middle, worst) {
         
+            $log.debug("best case: "+best);
+            $log.debug("middle case: "+middle);
+            $log.debug("worst case: "+worst);
+
+            $scope.showEstimation = true;
+            return $scope.estimation = (best +
+                worst + 2 * middle) / 4;
+        }
+
+        $scope.clearEstimation = function () {
+            $scope.bestCaseGuess = 0.0;
+            $scope.middleGroundGuess = 0.0;
+            $scope.worstCaseGuess = 0.0;
+            $scope.estimation = 0.0;
+            $scope.showEstimation = false;
+        }
     });
 /*
 Copyright 2016 Google Inc. All Rights Reserved. 
